1822ed72dbe84e9cf62664f455de84059b76611b
wzheng10 <wzheng100@gmail.com>
2021-03-16 04:53:40 -0700

    Error Handling HW

---
+++ W4D2/Error_handling_funtime/super_useful.rb
@@ -0,0 +1,70 @@
+# PHASE 2
+# def convert_to_int(str)
+#   Integer(str)
+# end
+
+# PHASE 3
+FRUITS = ["apple", "banana", "orange"]
+#if it is not the fruits, then run error
+class CoffeeError < StandardError
+  def message
+    "Thanks for the coffee. I guess you can try again"
+  end
+end
+
+class NotAFruitError < StandardError
+  def message
+    "Sorry, but that is not a fruit."
+
+  end
+end
+def reaction(maybe_fruit)
+  if FRUITS.include? maybe_fruit
+    puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit == "coffee"
+    raise CoffeeError
+  else 
+    raise NotAFruitError 
+  end 
+end
+
+def feed_me_a_fruit
+  puts "Hello, I am a friendly monster. :)"
+  begin
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue CoffeeError => e
+    puts e.message
+    retry
+  rescue NotAFruitError => e
+    puts e.message
+  end
+end  
+
+# PHASE 4
+class BestFriend
+  def initialize(name, yrs_known, fav_pastime)
+    raise ArgumentError.new(" 'name' of your best friend cannot be blank!") if name.empty?
+    raise ArgumentError.new(" 'yrs_known must be greater than or equal to 5!") if yrs_known.to_i < 5
+    raise ArgumentError.new(" 'fav_pastime' with your best friend cannot be blank!") if fav_pastime.empty?
+    
+    @name = name
+    @yrs_known = yrs_known.to_i
+    @fav_pastime = fav_pastime
+  end
+
+  def talk_about_friendship
+    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+  end
+
+  def do_friendstuff
+    puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
+  end
+
+  def give_friendship_bracelet
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+  end
+end
+
+
